c5bcbeaf58fe976d77d20faba6b86fddb737cb49
Yuhang <abeyuhang@gmail.com>
2019-03-21 11:50:37 +0800

    use vmWatch

--- 07-simple-todos/index.js
+++ 07-simple-todos/index.js
@@ -1,7 +1,9 @@
 import {
   Component,
   bootstrap,
-  VM
+  VM,
+  vmWatch,
+  vmUnwatch
 } from 'jinge';
 
 import _tpl from './app.html';
@@ -35,19 +37,31 @@ class App extends Component {
     super(args);
     this.todos = VM(loadTodos() || [createTodo('test')]);
     this.allDone = this.todos.length > 0 && !this.todos.find(t => !t.done);
+    this._updateHandler = this.update.bind(this);
+    /**
+     * 使用 vmWatch 在某些场景下可以使得代码更简洁。
+     * 但通常情况下，并不推荐使用 vmWatch 的形式，因为当模块化和逻辑性更加复杂
+     * 时，使用 watch 的模式不利于代码的可理解性。
+     * 
+     * 此处只是演示 vmWatch 函数的用法，并不推荐这样使用。
+     */
+    vmWatch(this, 'todos.**', this._updateHandler);
+  }
+  beforeDestroy() {
+    vmUnwatch(this, 'todos.**', this._updateHandler);
   }
   toggleAllDone() {
     this.todos.forEach(t => t.done = !this.allDone);
-    this.update();
+    // this.update(); // 如果不使用 vmWatch 来统一监控，则需要在每一处手动触发 this.update()
   }
   toggleDone(todo) {
     todo.done = !todo.done;
-    this.update();
+    // this.update(); // 如果不使用 vmWatch 来统一监控，则需要在每一处手动触发 this.update()
   }
   remove(todo) {
     const idx = this.todos.indexOf(todo);
     if (idx >= 0) this.todos.splice(idx, 1);
-    this.update();
+    // this.update(); // 如果不使用 vmWatch 来统一监控，则需要在每一处手动触发 this.update()
   }
   modify(todo) {
     const oldTitle = todo.title;
@@ -56,7 +70,7 @@ class App extends Component {
       return;
     }
     todo.title = title;
-    this.update();
+    // this.update(); // 如果不使用 vmWatch 来统一监控，则需要在每一处手动触发 this.update()
   }
   add() {
     const title = prompt("Please enter title of new todo");
@@ -64,16 +78,17 @@ class App extends Component {
       return;
     }
     this.todos.push(createTodo(title));
-    this.update();
+    // this.update(); // 如果不使用 vmWatch 来统一监控，则需要在每一处手动触发 this.update()
   }
   clear() {
     if (this.todos.length === 0) return;
     if (confirm('Sure to clear all todos?')) {
       this.todos.length = 0;
-      this.update();
+      // this.update(); // 如果不使用 vmWatch 来统一监控，则需要在每一处手动触发 this.update()
     }
   }
   update() {
+    console.log('updated');
     this.allDone = this.todos.length > 0 && !this.todos.find(t => !t.done);
     saveTodos(this.todos);
   }
