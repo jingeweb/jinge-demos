5f77ae3c57ccb02e1b308e0c8a6702fa25a69bd1
Yuhang <abeyuhang@gmail.com>
2019-03-26 17:38:07 +0800

    add ui-router transition hook example

--- 06-ui-router/components/hello.js
+++ 06-ui-router/components/hello.js
@@ -6,7 +6,15 @@ export default class Hello extends Component {
   static get template() {
     return '<h4>Hello</h4><p>hello, jinge!</p>';
   }
+  afterRender() {
+    this._des = router.transitionService.onBefore({
+      from: 'hello.**'
+    }, trans => {
+      return confirm('确认要退出？');
+    });
+  }
   beforeDestroy() {
+    this._des();
     console.log('Hello component will be destroy');
   }
 }
