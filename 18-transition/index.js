2e2d364694843b657df61f0d3c993f0c0a90e1e9
Yuhang <abeyuhang@gmail.com>
2019-04-10 18:09:40 +0800

    add transition/animation demo

---
+++ 18-transition/index.js
@@ -0,0 +1,27 @@
+import {
+  Component,
+  bootstrap
+} from 'jinge';
+
+import _tpl from './app.html';
+
+class App extends Component {
+  static get template() {
+    return _tpl;
+  }
+  constructor(args) {
+    super(args);
+    this.fade = false;
+    this.slide = true;
+    this.show = true;
+    this.si = 0;
+  }
+  handleTransition(action, className, el) {
+    console.log(action, className, el.className);
+  }
+  handleIfTransition(action, el) {
+    console.log(action, el.className);
+  }
+}
+
+bootstrap(App, document.getElementById('root-app'));
