fffce4ed83178fc1e35b4a271122788fe1533fc3
YUHANG GE <abeyuhang@gmail.com>
2020-09-03 11:07:52 +0800

    update

--- 20-route-i18n-dynamic-import/webpack.config.js
+++ 20-route-i18n-dynamic-import/webpack.config.js
@@ -17,15 +17,17 @@ module.exports = {
   node: false,
   devtool: prod ? false : 'source-map',
   plugins: [ new JingeWebpackPlugin({
-    // 如果使用了 webpack 的 dynamic import 来拆分代码，需要配置此参数为 true
-    multiChunk: true,
-    // 将 chunk 信息写入到指定的文件，方便加载到浏览器里配合 ResourceLoader 使用
-    writeChunkInfo: 'chunk.json',
+    chunk: {
+      // 如果使用了 webpack 的 dynamic import 来拆分代码，需要配置此参数为 true。默认为 false。
+      multiple: true,
+      // 将 chunk 信息写入到指定的文件，方便加载到浏览器里配合 ResourceLoader 使用。默认为 null，即不写入文件。
+      writeInfo: 'chunk.json',
+      // 是否将 webpack 抽取的模块共用的公共 chunk 信息也写入 writeInfo 指定的文件。默认为 false。
+      // includeCommon: false
+    },
     style: {
       // 是否保留注释，默认为 false。
       // keepComment: false,
-      // 渲染到 html 元素上的 attribute 的前缀，默认为 “_”。
-      // attrPrefix: '_',
       // 是否将 component style 提取并合并到一个文件里，默认为 false。
       extract: true
     },
